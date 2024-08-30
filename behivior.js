a885c12e1f568231882807d519f39c7bfbc1778a
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-30 18:22:23 -0400

    0.0.5

--- behivior.js
+++ behivior.js
@@ -1,6 +1,5 @@
 // @ts-check
-import { BeHive, seed } from 'be-hive/be-hive.js';
-import { MountObserver } from 'mount-observer/MountObserver.js';
+import { BeHive, seed, MountObserver } from 'be-hive/be-hive.js';
 /** @import {EMC} from './ts-refs/trans-render/be/types.d.ts' */
 /** @import {Actions, PAP,  AP} from './ts-refs/be-gingerly/types' */;
 
